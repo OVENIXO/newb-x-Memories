287a1bdfb0c86890af10cb33936b993fdca10514
OVENIXO <tkygamer1@gmail.com>
2024-10-05 00:10:28 +0300

    Add files via upload

--- src/newb/config.h
+++ src/newb/config.h
@@ -78,8 +78,8 @@
 
 /* Rainbow */ 
 //#define NL_RAINBOW         // [toggle] enable rainbow in sky
-#define NL_RAINBOW_CLEAR 1.2 // 0.3 subtle ~ 1.7 bright during clear
-#define NL_RAINBOW_RAIN 1.0  // 0.5 subtle ~ 2.0 bright during rain
+//#define NL_RAINBOW_CLEAR 1.2 // 0.3 subtle ~ 1.7 bright during clear
+//#define NL_RAINBOW_RAIN 1.0  // 0.5 subtle ~ 2.0 bright during rain
 
 /* Ore glow intensity */ 
 #define NL_GLOW_TEX 4.0    // 0.4 weak ~ 8.0 bright
@@ -126,7 +126,7 @@
 /* Rounded cloud Settings */ 
 #define NL_CLOUD2_THICKNESS 2.8      // 0.5 slim ~ 5.0 fat
 #define NL_CLOUD2_RAIN_THICKNESS 3.3 // 0.5 slim ~ 5.0 fat
-#define NL_CLOUD2_STEPS 7            // 3 low quality ~ 16 high quality
+#define NL_CLOUD2_STEPS 5            // 3 low quality ~ 16 high quality
 #define NL_CLOUD2_SCALE 0.027        // 0.003 large ~ 0.3 tiny
 #define NL_CLOUD2_SHAPE 0.66          // 0.0 round ~ 1.0 box
 #define NL_CLOUD2_DENSITY 24.0       // 1.0 blurry ~ 100.0 sharp
@@ -141,16 +141,6 @@
 #define NL_AURORA_COL1 vec3(1.0,0.96,0.52)
 #define NL_AURORA_COL2 vec3(0,1,0.39)
 
-/* Shooting star */ 
-#define NL_SHOOTING_STAR 1.0        // [toggle] 0.2 dim ~ 1.0 bright
-#define NL_SHOOTING_STAR_PERIOD 6.0 // 0.4 fast ~ 12.0 slow (12 secs)
-#define NL_SHOOTING_STAR_DELAY 64.0  // 0.0 no delay ~ 100.0 long time (100 secs)
-
-/* Rain effects */ 
-#define NL_RAIN_MIST_OPACITY 0.09 // [toggle] 0.04 very subtle ~ 0.5 thick mist blow
-#define NL_RAIN_WETNESS 0.8       // 0.0 no wetness - 1.0 fully wet blocks
-#define NL_RAIN_PUDDLES 0.9       // 0.0 no puddles - 1.0 puddles
-
 /* Chunk loading slide in animation */ 
 #define NL_CHUNK_LOAD_ANIM 100.0 // [toggle] -600.0 fall from top ~ 600.0 rise from bottom
 
@@ -162,7 +152,7 @@
 //#define NL_GODRAY 0.3 // [toggle] 0.1 subtle ~ 0.8 strong
 
 /* Sky reflection */ 
-//#define NL_GROUND_REFL 0.4       // [toggle] 0.2 slightly reflective ~ 1.0 fully reflect sky 
+#define NL_GROUND_REFL 0.4       // [toggle] 0.2 slightly reflective ~ 1.0 fully reflect sky 
 #define NL_GROUND_RAIN_WETNESS 1.0 // 0.0 no wetness ~ 1.0 fully wet blocks when raining
 #define NL_GROUND_RAIN_PUDDLES 0.7 // 0.0 no puddles ~ 1.0 puddles
 //#define NL_GROUND_AURORA_REFL    // [toggle] aurora reflection on ground
@@ -179,6 +169,9 @@
   Subpack names and flags are inside `pack_config.toml`.
   Build tool will enable corresponding flags when compiling. 
 */
+#ifdef DEFAULT
+
+#endif
 
 #ifdef CLOUDS_EXTENSION
   #undef NL_CLOUD2_MULTILAYER
@@ -210,23 +203,6 @@
 #ifdef SOFT_CLOUDS
   #undef NL_CLOUD_TYPE
   #define NL_CLOUD_TYPE 1
-  #undef NL_CLOUD2_MULTILAYER
 #endif
 
-#ifdef SOFT_CLOUDS
-  #undef NL_CLOUD_TYPE
-  #define NL_CLOUD_TYPE 1
-#endif
-
-#ifdef AROURA_SETTINGS
-  #undef NL_AURORA
-  #define NL_AURORA 1.2
-  #undef NL_AURORA_VELOCITY
-  #define NL_AURORA_VELOCITY 0.03
-  #undef NL_AURORA_SCALE
-  #define NL_AURORA_SCALE 0.04
-  #undef NL_AURORA_WIDTH
-  #define NL_AURORA_WIDTH 0.18
-#endif
-
-#endif
+#endif 
